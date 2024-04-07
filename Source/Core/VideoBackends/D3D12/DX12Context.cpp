036186f606cda5ba4cf4ac8a34f9ee9c348e847b
= <danworley@gmail.com>
2024-04-07 12:36:02 -0400

    Fix change from previous commit

--- Source/Core/VideoBackends/D3D12/DX12Context.cpp
+++ Source/Core/VideoBackends/D3D12/DX12Context.cpp
@@ -383,7 +383,7 @@ bool DXContext::CreateGXRootSignature()
     param_count++;
   }
 
-  // SetRootParamCBV(&params[param_count], 2, D3D12_SHADER_VISIBILITY_PIXEL);
+  SetRootParamCBV(&params[param_count], 2, D3D12_SHADER_VISIBILITY_PIXEL);
   param_count++;
 
   return BuildRootSignature(m_device.Get(), &m_gx_root_signature, params.data(), param_count);
